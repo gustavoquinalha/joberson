25bb9c6ea07479fc457a5db10f08577d593477fc
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2018-10-27 06:42:55 -0300

    add editable to box component roles & contact

--- components/BoxComponentContact.vue
+++ components/BoxComponentContact.vue
@@ -1,20 +1,69 @@
 <template>
-  <div>
+  <div class="box-component-contact">
     <span class="title">Contato</span>
     <ul class="list-style-none contact">
-      <li>{{ url }}</li>
-      <li>{{ contact }}</li>
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        <a v-if="item.type == 'url'" :href="item.text">{{ item.text }}</a>
+        <span v-else>{{ item.text }}</span>
+      </li>
     </ul>
+    <div class="addItem">
+      <input
+        type="text"
+        placeholder="Presison ENTER para adicionar"
+        @keydown.13="add"
+        v-model="contact"
+        @blur="contact = ''"
+      >
+    </div>
   </div>
 </template>
 
 <script>
+
+const REGEXP = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm)
+const getType = (value) => value.match(REGEXP) ? 'url' : 'email'
+
 export default {
   data() {
     return {
-      url: 'https://jobs.netflix.com/',
-      contact: 'jobs@netflix.com.br',
+      contact: '',
+      items: [{
+        text: 'https://jobs.netflix.com/',
+        type: 'url'
+      }, {
+        text: 'jobs@netflix.com.br',
+        type: 'email'
+      }]
+    }
+  },
+  methods: {
+    add() {
+      if (this.contact) {
+        this.items.push({
+          text: this.contact,
+          type: getType(this.contact)
+        })
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+  .box-component-contact {
+    .addItem {
+      opacity: 0;
+      transition: opacity .3s;
+    }
+
+    &:hover {
+      .addItem {
+        opacity: 1;
+      }
+    }
+  }
+</style>
--- components/BoxComponentRoles.vue
+++ components/BoxComponentRoles.vue
@@ -2,7 +2,19 @@
   <div>
     <span class="title">Vaga de</span>
     <br>
-    <span class="role">{{role}}</span>
+    <span
+      class="role" 
+      v-if="!edit"
+      @dblclick="toggleEdit(1)"
+    >{{ role }}</span>
+    <input
+      v-else
+      type="text"
+      ref="input"
+      v-model="role"
+      placeholder="Cargo"
+      @blur="toggleEdit(0)"
+      @keydown.13="toggleEdit(0)">
   </div>
 </template>
 
@@ -10,7 +22,18 @@
 export default {
   data() {
     return {
-      role: 'User Interface Designer'
+      role: 'User Interface Designer',
+      edit: false
+    }
+  },
+  methods: {
+    toggleEdit(value) {
+      this.edit = value
+      if (value) {
+        this.$nextTick(() => {
+          this.$refs.input.focus()
+        })
+      }
     }
   }
 }
